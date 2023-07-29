1a657fee05fecf401e2aaae115c9b16063c86790
Kamila Adamska <121kama121@gmail.com>
2023-07-29 23:10:57 +0200

    Fix positions in home styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,14 +5,16 @@ import css from './home.module.css';
 const Home = () => {
   return (
     <div className={css.homeBox}>
-      <img src={logo} alt="Phonebook logo" width="200" />
-      <h1 className={css.slogan}>
-        Keep your contacts safe and organize them easily!
-      </h1>
-      <p className={css.logOrReg}>
-        <Link className={css.link}>Login</Link> or{' '}
-        <Link className={css.link}>register</Link> now!
-      </p>
+      <img src={logo} alt="Phonebook logo" width="200" className={css.logo} />
+      <div>
+        <h1 className={css.slogan}>
+          Keep your contacts safe and organize them easily!
+        </h1>
+        <p className={css.logOrReg}>
+          <Link className={css.link}>Login</Link> or{' '}
+          <Link className={css.link}>register</Link> now!
+        </p>
+      </div>
     </div>
   );
 };
